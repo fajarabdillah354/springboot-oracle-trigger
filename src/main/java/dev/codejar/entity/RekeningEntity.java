0c8abf915676a70008e4ea0a9c8154c37899c510
fajarabdillah <fajardillah25@gmail.com>
2025-03-18 22:38:19 +0700

    Upload

---
+++ src/main/java/dev/codejar/entity/RekeningEntity.java
@@ -0,0 +1,44 @@
+package dev.codejar.entity;
+
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rekening")
+public class RekeningEntity {
+
+    @Id
+    @Column
+    private Long nomorRekening;
+
+    @Column(name = "nama_nasabah", nullable = false, length = 50)
+    private String namaNasabah;
+
+    @Column(name = "tempat_lahir", nullable = false, length = 20)
+    private String tempatLahir;
+
+
+    @Column(name = "tanggal_lahir")
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
+
+
+    @Column(name = "alamat", length = 50)
+    private String alamat;
+
+
+    @Column(name = "saldo", nullable = false)
+    private Double saldo = 0.0;
+
+    @OneToMany(mappedBy = "rekeningPengirim")
+    private List<TransactionEntity> transferOut;
+
+    @OneToMany(mappedBy = "rekeningPenerima")
+    private List<TransactionEntity> transferIn;
+
+}
